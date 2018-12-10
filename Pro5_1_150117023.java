f24daee7fca9170b2e90ba9754f400d6cfc1887d
Anıl Şenay <anilsenay3@gmail.com>
2018-12-10 07:57:15 +0300

    Pro5 v.0 added

--- Pro5_1_150117023.java
+++ Pro5_1_150117023.java
@@ -11,16 +11,24 @@ import java.util.Scanner;
  */
 
 public class Pro5_1_150117023 {
+
     	public static void main(String[] args){
 		Scanner input = new Scanner(System.in); 
 		int[] summaryOfTenDices = new int[51]; //the array which keeps number of times each total occurs.
-
+		System.out.println("Welcome to dice simulator program.");
 		System.out.print("Enter a value: ");
-		int howManyTimes = Integer.parseInt(args[0]);//input.nextInt();
+		int howManyTimes = input.nextInt();
 		rollingDice(howManyTimes, summaryOfTenDices);
 		printResult(summaryOfTenDices);
     	}
+	
+	/**
+	This method rolls 10 dices n times.
+	All 10 dice adding and result keeping in array as array[summary - 10]
+	After adding each 10 dice, temporary summary integer value setting 0 again for new roll.
 
+	Method takes 2 parameter as an integer n value and an integer type array.
+	*/
 	public static void rollingDice(int n, int sumArray[]){
                 int sum = 0;
 		for(int i = 0; i < n; i++){
@@ -34,7 +42,11 @@ public class Pro5_1_150117023 {
 			sum = 0;
 		}
 	}
+	/**
+	This method prints result of array which keeps dice summaries.
 
+	Method takes 1 parameter as an integer type array.
+	*/
 	public static void printResult(int sumArray[]){
 		for(int i = 10; i <= 60; i++){
 			System.out.print(i +": ");
--- Pro5_2_150117023.java
+++ Pro5_2_150117023.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +10,68 @@
  * @author anil
  */
 public class Pro5_2_150117023 {
-    
+
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a string: ");
+		String inputString = input.nextLine();
+
+                int arraySize = defineArraySize(inputString);
+
+		int inputArray[] = new int[arraySize];
+
+		stringToArray(inputString, inputArray);
+
+		int[][] updatedTable = new int[arraySize][arraySize];
+		fillUpdatedTable(updatedTable, inputArray);
+		for(int i = 0; i < updatedTable.length; i++){
+			for(int j = 0; j < updatedTable[i].length; j++)
+				System.out.print(updatedTable[i][j]);
+			System.out.println();
+		}
+	}
+
+	public static int defineArraySize(String str) {
+		int arraySize = 1;
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == '-')
+				arraySize++;
+		}
+		return arraySize;
+	}
+	public static void stringToArray(String str, int[] inputArray){
+		int a = 0;
+		int indexArray = 0;
+		for(int i = 0; i < inputArray.length+1; i++){
+			if(((int)str.indexOf('-', a) != -1)){
+			inputArray[indexArray] = Integer.parseInt( str.substring( a, str.indexOf('-', a) ) );
+			a = str.indexOf('-', a)+1;
+
+			indexArray++;
+			}
+			else{
+				inputArray[indexArray] = Integer.parseInt( str.substring(a, str.length()) );
+			}
+		}
+
+	}
+
+	public static int[] convertDectoB(int number, int size){
+		int[] numberAsBinary = new int[size];
+		int indexNo = size-1;
+		while(number >= 1){
+			numberAsBinary[indexNo] = number % 2;
+			number = number/2;
+			indexNo--;
+		}
+		return numberAsBinary;
+	}
+
+	public static void fillUpdatedTable(int[][] TwoDTable, int[] OneDArray){
+		for(int i = 0; i < TwoDTable.length; i++){
+			TwoDTable[i] = convertDectoB(OneDArray[i], OneDArray.length);
+		}
+	}
 }
